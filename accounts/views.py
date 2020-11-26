8d4d6eeabc0009ef8565a0ac07e453b91d05f1da
shoury <shanuu12e@gmail.com>
2020-11-26 10:47:27 +0530

    Much more changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth import login,logout
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponseRedirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_text
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from .tokens import account_activation_token
+from django.template.loader import render_to_string
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
