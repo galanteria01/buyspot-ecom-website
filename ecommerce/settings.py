7dace816b25d019330b7c24dab2ceeb64080c114
shoury <shanuu12e@gmail.com>
2020-12-01 22:05:57 +0530

    hosting bug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,6 +17,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
