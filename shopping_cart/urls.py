410bca57ce782be12a35fd1ce7385663966acbba
galanteria01 <shanuu12e@gmail.com>
2020-12-16 05:04:05 +0000

    Made cart working and implemented payment method

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -5,7 +5,10 @@ app_name = "shopping_cart"
 
 urlpatterns = [ 
     path("add_to_cart/<int:item_id>",views.add_to_cart,name="add_to_cart"),
-    path("order_summary",views.order_summary,name="order_summary"),
-    path("remove_from_cart/<int:item_id>",views.delete_from_cart,name="remove_from_cart")
+    path("order_summary",views.order_summary,name="order_summary"), 
+    path("remove_from_cart/<int:item_id>",views.delete_from_cart,name="remove_from_cart"),
+    path("success",views.success,name="purchase_success"),
+    path("checkout",views.checkout,name="checkout"),
+    path("update_transaction/<int:token>",views.update_transactions_records,name="update_records"),
 
 ]
